eb79eefb5b72b090e0e71085c47d3bb83d441bf0
Arif <arifali123454150@gmail.com>
2019-08-01 23:48:16 +0530

    last but not least

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var schedule = require('node-schedule');
+var bodyparser = require('body-parser');
+var validation = require('./functions');
+
+router.use(bodyparser.json());
+// router.
 
 // Home page route.
-router.get('/', function (req, res) {
+router.post('/', function (req, res) {
+	var datedata = req.query.datedata;
+	var timedata = req.query.timedata;
+	console.log(datedata,timedata);
+	var dtv = validation.datetimevalidation(datedata,timedata);
+	dtv.then((message)=>{
+		console.log(message);
+	})
+	.catch((message)=>{
+		console.log(message);
+	});
+
+
+	var date = new Date();
+	console.log(date);
+
   res.send('home page');
 });
 
