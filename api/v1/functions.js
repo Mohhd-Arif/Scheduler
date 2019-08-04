ba4f052adda4392825e98fc66e228ab965a5aec0
Arif <arifali123454150@gmail.com>
2019-08-04 15:58:21 +0530

    last but not the least

--- api/v1/functions.js
+++ api/v1/functions.js
@@ -12,21 +12,21 @@ function datetimevalidation(datedata,timedata){
 
 				if(!timedata[2]){
 					timedata[2]=0;
-				};console.log(timedata);
+				};
 				if(timedata[0]<0||timedata[0]>24||timedata[1]<0||timedata[1]>60||timedata[2]<0||timedata[2]>60){
 					reject("date format is not correct like -- 24hours 60 minute 60 second");
 				}
 				else{
 					date.setHours(timedata[0],timedata[1],timedata[2]);
-					console.log("time is set on ",date);
+					// console.log("time is set on ",date);
 				}
-				Date.prototype.isValid = function(){
+				Date.prototype.isValid = function(){//check for a valit time input
 					return this.getTime()===this.getTime();
 				}
 
 				if(date.isValid()){
 					let x = new Date();
-					console.log("new -----------------",x);
+					// console.log("new -----------------",x);
 						if(x>date){
 							reject("the time is in past");
 						}else{
@@ -37,9 +37,25 @@ function datetimevalidation(datedata,timedata){
 					}
 			}
 		}
+	});//promise ends
+}
+
+function id_validation(scheduled_work_list,del_job_id){
+	return new Promise((resolve,reject)=>{
+		// console.log("id_validation",scheduled_work_list,del_job_id);
+		if(del_job_id){
+			if(scheduled_work_list.hasOwnProperty(del_job_id)){
+				resolve(true);
+			}else{
+				reject("there is no such id found,it is an invalid id");
+			}
+		}else{
+			reject("enter an id first");
+		}
 	});
 }
 
 module.exports = {
 	datetimevalidation,
+	id_validation,
 }
--- api/v1/routes.js
+++ api/v1/routes.js
@@ -6,27 +6,57 @@ var validation = require('./functions');
 var scheduler = require('node-schedule');
 
 router.use(bodyparser.json());
+var scheduled_work_list = 0;
 
-// date and time setter
-router.post('/', function (req, res) {
+// schedule a work
+router.post('/schedulework',(req, res)=> {
 	var datedata = req.query.datedata;
 	var timedata = req.query.timedata;
 	// console.log(datedata,timedata);
-	var dtv = validation.datetimevalidation(datedata,timedata);
-	dtv.then((time)=>{
-		console.log(time);
-		var job = scheduler.scheduleJob(time,()=>console.log("i am doing your work at time",time));
-		res.send("your work is scheduled at "+time);
+	var root = validation.datetimevalidation(datedata,timedata);
+	root.then((date)=>{
+		// console.log(time);
+		var job = scheduler.scheduleJob(date,()=>console.log("i am doing your work at time",date));
+		scheduled_work_list = scheduler.scheduledJobs;
+		res.send("your work is scheduled at "+date+Object.keys(scheduled_work_list));
 	})
 	.catch((message)=>{
-		console.log(message);
+		// console.log(message);
 		res.send(message);
 	});
 });
 
-// About page route.
-router.get('/about', function (req, res) {
-  res.send('About page');
+// delete a scheduled work
+router.delete('/delete_scheduledwork',(req, res)=> {
+  // console.log(scheduled_work_list);
+	// console.log(Object.keys(scheduled_work_list));
+	var del_job_id = req.query.del_job_id;
+	var root = validation.id_validation(scheduled_work_list,del_job_id);
+	root.then((bool)=>{
+		if(bool){
+			scheduled_work_list[del_job_id].cancel();
+		}
+		res.send(del_job_id+" is been deleted \n remaining tasks "+scheduled_work_list);
+	}).catch((message)=>{
+		res.send(message);
+	})
 });
 
+router.patch('/update_scheduledwork',(req,res)=>{
+	var datedata = req.query.datedata;
+	var timedata = req.query.timedata;
+	var del_job_id = req.query.del_job_id;
+	var root = validation.id_validation(scheduled_work_list,del_job_id);
+	var root1 = validation.datetimevalidation(datedata,timedata);
+	Promise.all([root,root1]).then((date)=>{
+		console.log(Object.keys(date));
+		scheduled_work_list[del_job_id].reschedule(date[1]);
+		res.send("your schedule having id=>"+del_job_id+"is updated");
+	}).catch((message)=>{
+		res.send(message);
+	});
+});
+
+
+router.patch('/edit_schedule')
 module.exports = router;
